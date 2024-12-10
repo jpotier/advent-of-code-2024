8967e539c5aa8540506f75c5c6928bca01ee5739
Samae <eeva@samae>
2024-12-10 19:49:44 +0200

    Reenable all days

--- Main.hs
+++ Main.hs
@@ -12,15 +12,15 @@ import Day6
 main :: IO ()
 main = do
   putStrLn "AoC 2024"
-  --putStrLn "Day 1"
-  --Day1.main
-  --putStrLn "Day 2"
-  --Day2.main
-  --putStrLn "Day 3"
-  --Day3.main
-  --putStrLn "Day 4"
-  --Day4.main
-  --putStrLn "Day 5"
-  --Day5.main
+  putStrLn "Day 1"
+  Day1.main
+  putStrLn "Day 2"
+  Day2.main
+  putStrLn "Day 3"
+  Day3.main
+  putStrLn "Day 4"
+  Day4.main
+  putStrLn "Day 5"
+  Day5.main
   putStrLn "Day 6"
   Day6.main
